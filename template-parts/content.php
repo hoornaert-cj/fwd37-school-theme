18ef2e3fafc65e2640835512954617948ecac7be
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2024-05-21 14:30:44 -0700

    Changed the content display to excerpt for blog posts

--- template-parts/content.php
+++ template-parts/content.php
@@ -31,31 +31,9 @@
 
 	<?php fwd37_school_theme_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'fwd37-school-theme' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'fwd37-school-theme' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
+	<div class="entry-summary">
+        <?php the_excerpt(); ?>
+    </div><!-- .entry-summary -->
 
 	<footer class="entry-footer">
 		<?php fwd37_school_theme_entry_footer(); ?>
