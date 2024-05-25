c0f428e71a00dde9c7de71c5e1f5f522a7b029c8
J-H-Seccia <jacob.cameron.harris@gmail.com>
2024-05-24 17:44:38 -0700

    added student CPT, locked move, delete and add functionalities from the block editor.

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -1,6 +1,7 @@
 <?php
 
 function fwd_register_custom_post_types() {
+    //Staff CPT
     $labels = array(
         'name'                  => _x( 'Staff', 'post type general name' ),
         'singular_name'         => _x( 'Staff', 'post type singular name' ),
@@ -48,6 +49,57 @@ function fwd_register_custom_post_types() {
     );
 
     register_post_type( 'fwd-staff', $args );
+
+    //Student CPT
+    $labels = array(
+        'name'                  => _x( 'Students', 'post type general name' ),
+        'singular_name'         => _x( 'Student', 'post type singular name' ),
+        'menu_name'             => _x( 'Student', 'admin menu' ),
+        'name_admin_bar'        => _x( 'Student', 'add new on admin bar' ),
+        'add_new'               => _x( 'Add New', 'student' ),
+        'add_new_item'          => __( 'Add New Student' ),
+        'new_item'              => __( 'New Student' ),
+        'edit_item'             => __( 'Edit Student' ),
+        'view_item'             => __( 'View Student' ),
+        'all_items'             => __( 'All Student' ),
+        'search_items'          => __( 'Search Student' ),
+        'parent_item_colon'     => __( 'Parent Student:' ),
+        'not_found'             => __( 'No student found.' ),
+        'not_found_in_trash'    => __( 'No student(s) found in Trash.' ),
+        'archives'              => __( 'Student Archives' ),
+        'insert_into_item'      => __( 'Insert into student' ),
+        'uploaded_to_this_item' => __( 'Uploaded to this student' ),
+        'filter_items_list'     => __( 'Filter student list' ),
+        'items_list_navigation' => __( 'Student list navigation' ),
+        'items_list'            => __( 'Student list' ),
+        'featured_image'        => __( 'Student featured image' ),
+        'set_featured_image'    => __( 'Set student featured image' ),
+        'remove_featured_image' => __( 'Remove student featured image' ),
+        'use_featured_image'    => __( 'Use as featured image' ),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_nav_menus'  => true,
+        'show_in_admin_bar'  => true,
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'students' ),
+        'capability_type'    => 'post',
+        'has_archive'        => false,
+        'hierarchical'       => false,
+        'menu_position'      => 6,
+        'menu_icon'          => 'dashicons-admin-users',
+        'supports'           => array( 'title', 'editor' ),
+        'template'           => array( array( 'core/paragraph', array( 'placeholder' => 'Describe the sudent...' )  ), array( 'core/button' ) ),
+        'template_lock'      => 'all',
+    );
+
+    register_post_type( 'fwd-student', $args );
 }
 add_action( 'init', 'fwd_register_custom_post_types' );
 
@@ -99,6 +151,16 @@ function fwd_change_staff_title_placeholder( $title ) {
 
     return $title;
 }
+function fwd_change_student_title_placeholder( $title ) {
+    $screen = get_current_screen();
+
+    if ( 'fwd-student' == $screen->post_type ) {
+        $title = 'Add student name';
+    }
+
+    return $title;
+}
 add_filter( 'enter_title_here', 'fwd_change_staff_title_placeholder' );
+add_filter( 'enter_title_here', 'fwd_change_student_title_placeholder' );
 
 ?>
