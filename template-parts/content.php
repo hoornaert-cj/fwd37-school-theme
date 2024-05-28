02e19b0b8cba85c778e16701e8923de3cf3b4ab5
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2024-05-28 16:17:02 -0700

    Updated content.php so that all of the post's text displays instead of an excerpt

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,33 +9,39 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>data-aos="fade-up">
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> data-aos="fade-up">
+    <header class="entry-header">
+        <?php
+        if ( is_singular() ) :
+            the_title( '<h1 class="entry-title">', '</h1>' );
+        else :
+            the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+        endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				fwd37_school_theme_posted_on();
-				fwd37_school_theme_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+        if ( 'post' === get_post_type() ) :
+            ?>
+            <div class="entry-meta">
+                <?php
+                fwd37_school_theme_posted_on();
+                fwd37_school_theme_posted_by();
+                ?>
+            </div>
+        <?php endif; ?>
+    </header>
 
-	<?php fwd37_school_theme_post_thumbnail(); ?>
+    <?php fwd37_school_theme_post_thumbnail(); ?>
 
-	<div class="entry-summary">
-        <?php the_excerpt(); ?>
-    </div><!-- .entry-summary -->
+    <?php if ( ! is_singular() ) : ?>
+        <div class="entry-summary">
+            <?php the_excerpt(); ?>
+        </div>
+    <?php else : ?>
+        <div class="entry-content">
+            <?php the_content(); ?>
+        </div>
+    <?php endif; ?>
 
-	<footer class="entry-footer">
-		<?php fwd37_school_theme_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+    <footer class="entry-footer">
+        <?php fwd37_school_theme_entry_footer(); ?>
+    </footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
