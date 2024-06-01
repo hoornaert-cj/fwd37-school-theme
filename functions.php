7b739653fab2318839f869fef0bfc251ae055729
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2024-06-01 15:22:39 -0700

    updated the url for the Fira Sans Google font

--- functions.php
+++ functions.php
@@ -168,7 +168,7 @@ function fwd37_school_theme_scripts() {
     }
 
     // Enqueue Google Font "Fira Sans"
-    wp_enqueue_style( 'fwd37-google-fonts', 'https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&display=swap', false );
+    wp_enqueue_style( 'fwd37-google-fonts', 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', false );
 }
 add_action( 'wp_enqueue_scripts', 'fwd37_school_theme_scripts' );
 
