35b7bbfb8a22dd7e442d925932ae67c46a6fe110
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2024-06-02 12:08:27 -0700

    added h1 text to the student page

--- page-students.php
+++ page-students.php
@@ -6,6 +6,7 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
+    <h1 class="page-title">The Class</h1>
     <?php
     // Modify excerpt length and more link text for this template
     function custom_student_excerpt_length($length) {
